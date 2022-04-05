6fed783860439add5f4849aa3dbca27dcc283faa
camilord-end <juliancaor10@gmail.com>
2022-04-05 14:08:30 -0500

    added spa functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from './components/Navbar/Navbar'
+import { Home } from "./components/Home/Home";
+import { Projects } from "./components/Projects/Projects";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact"
 
 export const App = () => {
   return (
     <div className="container">
-      <div className="this">This is</div>
-      <div className="template">a template</div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  )
-}
-
+  );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <h1 className="name">Julian Orozco</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects </Link>
+          </li>
+          <li>
+            <Link to="/about"> About </Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
