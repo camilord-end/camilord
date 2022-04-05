4c8733e1120591b53b4a0aff79d836517555f3ee
camilord-end <juliancaor10@gmail.com>
2022-04-04 19:18:14 -0500

    Project template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="this">This is</div>
+      <div className="template">a template</div>
+    </div>
+  )
+}
+
+export default App
