71fe8dfcf0fbf1813af9ea6883c0e98e619b7389
camilord-end <juliancaor10@gmail.com>
2022-04-06 11:27:57 -0500

    added dark mode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Navbar } from './components/Navbar/Navbar'
 import { Home } from "./components/Home/Home";
 import { Projects } from "./components/Projects/Projects";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact"
+import { FaMoon, FaSun } from "react-icons/fa";
+import useLocalStorage from "use-local-storage";
+
+import "./App.css";
 
 export const App = () => {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "ligth");
+
+  const handleDarkMode = () => {
+    const newTheme = theme === "ligth" ? "dark" : "ligth";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="container">
-      <Navbar />
+    <div className="container" data-theme={theme}>
+      <Navbar themeHandle={handleDarkMode}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <div className="theme-button" onClick={handleDarkMode}>
+        {theme==="dark"? <FaSun />: <FaMoon />}
+      </div>
     </div>
   );
 };
