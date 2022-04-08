f885c5d1f9582febcd2c7fd743d8e420bbd98a34
camilord-end <juliancaor10@gmail.com>
2022-04-07 21:32:30 -0500

    Added basic styles for navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,7 @@
-import Typical from "react-typical";
-
 export const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>I`m Julian Orozco</h1>
-      <h2>
-        <Typical
-          steps={[
-            "And i like to learn new tecnologies",
-            1000,
-            "And i like to transform ideas into code",
-            1000,
-          ]}
-          loop={2}
-          wrapper="p"
-        />
-      </h2>
       <img
         id="placeholder"
         src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png"
