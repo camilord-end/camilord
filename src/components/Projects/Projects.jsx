bba9408df0f22d93df61e82e94c8fb89f513b6ae
camilord-end <juliancaor10@gmail.com>
2022-04-18 18:21:16 -0500

    added projects basic styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,11 @@
 import ProjectData from "./ProjectData.json";
 import { ProjectCard } from "./ProjectCard";
 
+import "./Projects.css"
+
 export const Projects = () => {
   return (
-    <div>
+    <div className="projects-container">
       {ProjectData.map((project) => {
         return (
           <div className="project-card" key={project.id}>
