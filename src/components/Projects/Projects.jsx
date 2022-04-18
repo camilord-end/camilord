ddd6280bb476645d157770137b13afa031759035
camilord-end <juliancaor10@gmail.com>
2022-04-18 12:30:33 -0500

    added data imports and map

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,22 @@
+import ProjectData from "./ProjectData.json";
+import { ProjectCard } from "./ProjectCard";
+
 export const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div>
+      {ProjectData.map((project) => {
+        return (
+          <div className="project-card" key={project.id}>
+            <ProjectCard
+              description={project.description}
+              link={project.link}
+              name={project.name}
+              preview={project.preview}
+              stack={project.stack}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
