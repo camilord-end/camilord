55b6d98b769fdd652d4cf4d99cb20e7aecaa1ce2
camilord-end <juliancaor10@gmail.com>
2022-04-19 14:29:24 -0500

    added state to set a card open

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,21 +1,30 @@
-import "./ProjectCard.css"
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { Details } from "./Details.jsx";
+import "./ProjectCard.css";
 
 export const ProjectCard = ({ name, description, stack, link, preview }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <div>
-        <h1 className="project-name">{name}</h1>
-      </div>
-      <p className="project-description">{description}</p>
-      <ul className="project-stack">
-        {stack.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
-      <a href={`http://${link}`} target="_blank" rel="noreferrer">
-        Project Link
-      </a>
-      <img src={`http://${preview}`} alt="project preview" />
-    </>
+    <motion.li layout onClick={toggleOpen} initial={{ borderRadius: 10 }}>
+      <motion.div layout className="main-content">
+        <motion.h1 layout className="project-name">
+          {name}
+        </motion.h1>
+        <motion.div layout className="project-image-container">
+          <img src={`http://${preview}`} alt="project preview" />
+        </motion.div>
+      </motion.div>
+      <AnimatePresence>
+        {isOpen && (
+          <Details description={description} stack={stack} link={link} />
+        )}
+      </AnimatePresence>
+    </motion.li>
   );
 };
