57613e324900984f79b5cf7ee315feee0d04cda5
camilord-end <juliancaor10@gmail.com>
2022-04-19 14:30:16 -0500

    added framer motion for card animations

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,24 +1,26 @@
 import ProjectData from "./ProjectData.json";
 import { ProjectCard } from "./ProjectCard";
+import { motion } from "framer-motion";
 
-import "./Projects.css"
+import "./Projects.css";
 
 export const Projects = () => {
   return (
     <div className="projects-container">
-      {ProjectData.map((project) => {
-        return (
-          <div className="project-card" key={project.id}>
+      <motion.ul layout initial={{ borderRadius: 10 }}>
+        {ProjectData.map((project) => {
+          return (
             <ProjectCard
+              key={project.id}
               description={project.description}
               link={project.link}
               name={project.name}
               preview={project.preview}
               stack={project.stack}
             />
-          </div>
-        );
-      })}
+          );
+        })}
+      </motion.ul>
     </div>
   );
 };
