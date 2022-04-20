8fe1bfd6d320dbb082b1091a3d57eafede6fdc89
camilord-end <juliancaor10@gmail.com>
2022-04-20 14:09:41 -0500

    added repository / preview

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,9 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { Details } from "./Details.jsx";
+import { FaPlus, FaMinus } from "react-icons/fa";
 import "./ProjectCard.css";
 
-export const ProjectCard = ({ name, description, stack, link, preview }) => {
+export const ProjectCard = ({ name, description, stack, repository, preview, image }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
@@ -11,18 +12,23 @@ export const ProjectCard = ({ name, description, stack, link, preview }) => {
   };
 
   return (
-    <motion.li layout onClick={toggleOpen} initial={{ borderRadius: 10 }}>
+    <motion.li className="card" layout initial={{ borderRadius: 10 }}>
       <motion.div layout className="main-content">
-        <motion.h1 layout className="project-name">
-          {name}
-        </motion.h1>
         <motion.div layout className="project-image-container">
-          <img src={`http://${preview}`} alt="project preview" />
+          <img className="project-image" src={image} alt="project image" />
         </motion.div>
+        <div className="project-basic">
+          <motion.h1 layout className="project-name">
+            {name}
+          </motion.h1>
+          <motion.button layout onClick={toggleOpen}>
+            {isOpen ? <FaMinus /> : <FaPlus />}
+          </motion.button>
+        </div>
       </motion.div>
       <AnimatePresence>
         {isOpen && (
-          <Details description={description} stack={stack} link={link} />
+          <Details description={description} stack={stack} repository={repository} preview={preview} />
         )}
       </AnimatePresence>
     </motion.li>
