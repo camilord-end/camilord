83040397902ac26e919d644eb316453c777a4c83
camilord-end <juliancaor10@gmail.com>
2022-04-20 14:10:11 -0500

    added AnimatePresence for smother transitions

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,26 +1,29 @@
 import ProjectData from "./ProjectData.json";
 import { ProjectCard } from "./ProjectCard";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import "./Projects.css";
 
 export const Projects = () => {
   return (
     <div className="projects-container">
-      <motion.ul layout initial={{ borderRadius: 10 }}>
-        {ProjectData.map((project) => {
-          return (
-            <ProjectCard
-              key={project.id}
-              description={project.description}
-              link={project.link}
-              name={project.name}
-              preview={project.preview}
-              stack={project.stack}
-            />
-          );
-        })}
-      </motion.ul>
+      <AnimatePresence>
+        <motion.ul layout className="projects-wrap" initial={{ borderRadius: 10 }}>
+          {ProjectData.map((project) => {
+            return (
+              <ProjectCard
+                key={project.id}
+                description={project.description}
+                repository={project.repository}
+                name={project.name}
+                preview={project.preview}
+                stack={project.stack}
+                image={project.image}
+              />
+            );
+          })}
+        </motion.ul>
+      </AnimatePresence>
     </div>
   );
 };
