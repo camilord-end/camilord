71184b541979a39044783a97b75c5614cbc06465
camilord-end <juliancaor10@gmail.com>
2022-04-20 14:09:02 -0500

    added repository and preview

--- src/components/Projects/Details.jsx
+++ src/components/Projects/Details.jsx
@@ -1,22 +1,26 @@
 import { motion } from "framer-motion";
 
-export const Details = ({ description, stack, link }) => {
+export const Details = ({ description, stack, repository, preview }) => {
   return (
     <motion.div
       layout
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      
     >
-      <p className="project-description">{description}</p>
-      <ul className="project-stack">
+      <motion.p className="project-description">{description}</motion.p>
+      <motion.ul className="project-stack">
         {stack.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return <motion.li key={index}>{item}</motion.li>;
         })}
-      </ul>
-      <a href={`http://${link}`} target="_blank" rel="noreferrer">
-        Project Linkses
-      </a>
+      </motion.ul>
+      <motion.a href={`${repository}`} target="_blank" rel="noreferrer">
+        Project Repository
+      </motion.a>
+      <motion.a href={`${preview}`} target="_blank" rel="noreferrer">
+        Project Preview
+      </motion.a>
     </motion.div>
   );
 };
