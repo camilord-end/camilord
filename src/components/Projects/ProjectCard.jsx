3a2822f27c168969b7ef5e3ff5be0e2eba3907ea
camilord-end <juliancaor10@gmail.com>
2022-04-21 18:12:58 -0500

    Refactor  motion logic

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,14 @@ import { Details } from "./Details.jsx";
 import { FaPlus, FaMinus } from "react-icons/fa";
 import "./ProjectCard.css";
 
-export const ProjectCard = ({ name, description, stack, repository, preview, image }) => {
+export const ProjectCard = ({
+  name,
+  description,
+  stack,
+  repository,
+  preview,
+  image,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
@@ -14,21 +21,31 @@ export const ProjectCard = ({ name, description, stack, repository, preview, ima
   return (
     <motion.li className="card" layout initial={{ borderRadius: 10 }}>
       <motion.div layout className="main-content">
-        <motion.div layout className="project-image-container">
+        <div className="project-image-container">
           <img className="project-image" src={image} alt="project image" />
-        </motion.div>
+        </div>
         <div className="project-basic">
-          <motion.h1 layout className="project-name">
-            {name}
-          </motion.h1>
-          <motion.button layout onClick={toggleOpen}>
-            {isOpen ? <FaMinus /> : <FaPlus />}
-          </motion.button>
+          <div className="project-name">
+            <h1 className="project-name">{name}</h1>
+          </div>
+          <motion.div
+            whileHover={{ scale: 1.5 }}
+            whileTap={{ scale: 0.8 }}
+            className="project-details-button"
+            onClick={toggleOpen}
+          >
+            {isOpen ? <FaMinus alignmentBaseline="central" /> : <FaPlus />}
+          </motion.div>
         </div>
       </motion.div>
       <AnimatePresence>
         {isOpen && (
-          <Details description={description} stack={stack} repository={repository} preview={preview} />
+          <Details
+            description={description}
+            stack={stack}
+            repository={repository}
+            preview={preview}
+          />
         )}
       </AnimatePresence>
     </motion.li>
