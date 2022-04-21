1a7d361299394a34693d6fa4758f006f1091c8fc
camilord-end <juliancaor10@gmail.com>
2022-04-21 18:11:27 -0500

    Added icons to external links

--- src/components/Projects/Details.jsx
+++ src/components/Projects/Details.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import "./Details.css";
 
 export const Details = ({ description, stack, repository, preview }) => {
   return (
@@ -6,21 +8,35 @@ export const Details = ({ description, stack, repository, preview }) => {
       layout
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      
+      transition={{ delay: 0.2 }}
+      exit={{ opacity: 1 }}
     >
       <motion.p className="project-description">{description}</motion.p>
-      <motion.ul className="project-stack">
-        {stack.map((item, index) => {
-          return <motion.li key={index}>{item}</motion.li>;
-        })}
-      </motion.ul>
-      <motion.a href={`${repository}`} target="_blank" rel="noreferrer">
-        Project Repository
-      </motion.a>
-      <motion.a href={`${preview}`} target="_blank" rel="noreferrer">
-        Project Preview
-      </motion.a>
+      <div className="stack">
+        <motion.ul className="project-stack">
+          {stack.map((item, index) => {
+            return <motion.li key={index}>{item}</motion.li>;
+          })}
+        </motion.ul>
+      </div>
+      <motion.div layout className="links">
+        <a
+          className="github-link"
+          href={`${repository}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          className="project-link"
+          href={`${preview}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaExternalLinkAlt />
+        </a>
+      </motion.div>
     </motion.div>
   );
 };
