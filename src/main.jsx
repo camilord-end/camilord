cc4d540b2b81e1176fa6fef157add1cdcadc019d
camilord-end <juliancaor10@gmail.com>
2022-05-23 12:07:07 -0500

    React 18 update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  , document.getElementById("root"));
+)
