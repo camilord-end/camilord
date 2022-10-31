360d8fb5ba43f475948f6f436154cfce2904ba7a
Julian Orozco <juliancaor10@gmail.com>
2022-10-30 19:49:49 -0500

    reformat for i18n

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import './i18n'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
