c6264658c2035e6babf42eb2ec4511f68d9ce467
Julian Orozco <juliancaor10@gmail.com>
2022-11-29 18:37:16 -0500

    Add colors to navbar link icons

--- src/utils/linkItems.jsx
+++ src/utils/linkItems.jsx
@@ -1,21 +1,33 @@
 import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined'
 import PermMediaOutlinedIcon from '@mui/icons-material/PermMediaOutlined'
 import ConnectWithoutContactOutlinedIcon from '@mui/icons-material/ConnectWithoutContactOutlined'
+//Colors
+import { green, pink, yellow } from '@mui/material/colors'
 
 export const linkItems = [
   {
     name: 'Home',
-    icon: <CottageOutlinedIcon />,
+    icon: <CottageOutlinedIcon color={pink['A200']} htmlColor={pink['A200']} />,
     href: '/'
   },
   {
     name: 'Projects',
-    icon: <PermMediaOutlinedIcon />,
+    icon: (
+      <PermMediaOutlinedIcon
+        color={yellow['A700']}
+        htmlColor={yellow['A700']}
+      />
+    ),
     href: '/projects'
   },
   {
     name: 'Contact',
-    icon: <ConnectWithoutContactOutlinedIcon />,
+    icon: (
+      <ConnectWithoutContactOutlinedIcon
+        color={green[500]}
+        htmlColor={green[500]}
+      />
+    ),
     href: '/contact'
   }
 ]
