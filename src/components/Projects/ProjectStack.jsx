4d561d002e8593e12f5e948a61df61ad76b71931
Julian Orozco <juliancaor10@gmail.com>
2022-12-01 12:40:11 -0500

    Implement project stack cards

---
+++ src/components/Projects/ProjectStack.jsx
@@ -0,0 +1,26 @@
+import FullStack from '../../data/FullStack.json'
+
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+
+export const ProjectStack = ({ stack }) => {
+  return (
+    <Box sx={{ display: 'flex', mt: 3 }}>
+      {stack.map((item) => (
+        <Card key={item} sx={{ m: 1 }}>
+          <CardMedia
+            component='img'
+            src={FullStack[item]}
+            alt={`${item} icon`}
+            sx={{ height: 40, width: 40 }}
+          />
+          <Divider />
+          <Typography variant='caption'>{item}</Typography>
+        </Card>
+      ))}
+    </Box>
+  )
+}
