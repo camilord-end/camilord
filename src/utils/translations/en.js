59a8a6e16d82e7a050aa1c288e4ce42059f01dc4
Julian Orozco <juliancaor10@gmail.com>
2022-12-01 12:38:03 -0500

    Add project descriptions and translations

--- src/utils/translations/en.js
+++ src/utils/translations/en.js
@@ -4,25 +4,22 @@ export const en = {
       part1: 'Hi, i’m',
       part2: 'I build things for the web',
       part3:
-        'I’m a Front-end Developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products.',
+        'I’m a Front-end Developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, user-centered products.',
       part4:
-        'Hello! My name is Julian and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to study systems engineering, turns out that i really focused on the presentation of the product and this directed me towards Front-end Development',
+        'Hello! My name is Julian and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to study systems engineering, turns out that I really focused on the presentation of the product and this directed me towards Front-end Development',
       part5:
-        'First I started learning C++,Python and Java in the University. Worked hard to learn data structures with C++, and learned about algorithms mainly with Python. Also learned java to fully understand the SOLID principles and apply them.',
+        'First I started learning C++, Python and Java at the University. Worked hard to learn data structures with C++, and learned about algorithms mainly with Python. Also learned java to fully understand the SOLID principles and apply them.',
       part6:
         'When I found out that my passion was on Front-end development I started learning all about semantic Html, CSS and tools to make it better and easier, and also JavaScript to give it more functionality, then I started learning AngularJS but wast really into this framework so I started learning about React library, and this was the tool that I was most comfortable.',
       part7: 'I am proficient with:',
       part8: 'and familiar with:',
       part9:
-        'In my free time I like to play the guitar and listen to music, been doing it for 15 years, I also like folkloric percusion and the drums. Lately i have fallen in love with cats, right now I take care of one named Oreo, he is the funniest and smartest animal I have ever known.',
+        'In my free time I like to play the guitar and listen to music, been doing it for 15 years. I also like folkloric percussion and the drums. Lately I have fallen in love with cats, right now I take care of one named Oreo; he is the funniest and smartest animal I have ever known.',
       desc1: 'About Me',
       desc2: 'Skills',
       desc3: 'Hobbies'
     },
     contact: {
-      desc1: 'Sobre mi',
-      desc2: 'Habilidades',
-      desc3: 'Hobbies',
       part1: 'Contact Me:',
       part2: 'Email',
       part3: 'Phone'
@@ -34,6 +31,18 @@ export const en = {
       LightMode: 'Light Mode',
       DarkMode: 'Dark Mode',
       Language: 'Language'
+    },
+    projects: {
+      desc1:
+        "A blog involving cats, it doesn't get any better. Using NextJS as the main framework, I builded a robust page that actually uses GraphQL and Hygraph consecuently to manage all the content taking advantage off the Headless CMS model to keep all content and logic separated and making the process of adding new content easy and fast for both programmer and common users.",
+      desc2:
+        'A pokemon info database that contains the sprite images for said pokemon and it changes to its shiny variable on click, also contains info about Special Abilities, Types, and last but not least a table with stats info and how much effort will take to upgrade those stats.',
+      desc3:
+        'This project focuses on using OpenWeather API and OpenCountries API to practice conditional rendering and the presentation of all the info gathered through the API calls. All Css was done with TailwindCSS to get familiar with the framework.',
+      desc4:
+        'A todo list implementing custom hooks like useTodo, creating visibility filters using dispatch, and using useContext due to complex states, making use of dispatch instead of callbacks for better performance. Also using module prop types definitions for text imput.',
+      desc5:
+        'This real state project focuses on using the file-based routing provided by NextJS, making routing easier and better to understand, also uses Chakra UI to practice the library, also uses tools like nprogress on loading times to keep the user focused on the page. Focused on getting used to the getStaticProps and getServerSideProps Function to pass props to components in static or dinamic envoirements.'
     }
   }
 }
--- src/utils/translations/es.js
+++ src/utils/translations/es.js
@@ -4,9 +4,9 @@ export const es = {
       part1: 'Hola, soy',
       part2: 'Construyo cosas para la web',
       part3:
-        'Soy un desarrollador Frond-end especializándome en construir (y ocasionalmente diseñar) experiencias digitales excepcionales. Actualmente estoy enfocado en construir productos accesibles y enfocados en el usuario.',
+        'Soy un desarrollador Front-end especializándome en construir (y ocasionalmente diseñar) experiencias digitales excepcionales. Actualmente estoy enfocado en construir productos accesibles y enfocados en el usuario.',
       part4:
-        '¡Hola! mi nombre es Julián y disfruto crear cosas que se hospedan en el internet. Mi interés por el desarrollo web empezó en 2012 cuando decidí estudiar ingeniería de sistemas, descubrí que me enfocaba mucho en la presentación del producto y esto me dirigió hacia el desarrollo Front-end',
+        '¡Hola!, mi nombre es Julián y disfruto crear cosas que se hospedan en el internet. Mi interés por el desarrollo web empezó en 2012 cuando decidí estudiar ingeniería de sistemas, descubrí que me enfocaba mucho en la presentación del producto y esto me dirigió hacia el desarrollo Front-end',
       part5:
         'Empecé aprendiendo C++, Python y Java en la universidad. Trabajé fuertemente para aprender estructuras de datos en C++, aprendí algoritmia principalmente con Python y también entendí los principios SOLID y como aplicarlos gracias a Java.',
       part6:
@@ -31,6 +31,18 @@ export const es = {
       LightMode: 'Modo Claro',
       DarkMode: 'Modo Oscuro',
       Language: 'Lenguaje'
+    },
+    projects: {
+      desc1:
+        'Un blog sobre gatos, no hay nada mejor. Usando NextJS como framework principal, construí una página robusta que usa GraphQL y consecuentemente Hygraph para manejar todo el contenido aprovechando el modelo Headless CMS, manteniendo todo el contenido y la lógica del programa separados. Haciendo así que el proceso de añadir nuevo contenido sea fácil para el usuario, y añadir funcionalidades sea fácil para el desarrollador.',
+      desc2:
+        'Una base de datos de Pokémon que contiene imágenes de los sprites del correspondiente Pokémon, los cambian a su variante al dar clic en la imagen, también contiene información sobre las habilidades especiales, tipos, y de último, pero no menos importante, una tabla con las estadísticas y cuanto esfuerzo tomara subirlas.',
+      desc3:
+        'Este proyecto se enfoca en el uso de las APIs OpenWeather y OpenCountries, se practica el renderizado condicional mediante una barra de input que sirve como filtro. Se presenta toda la información del país y el clima de la capital respectiva. Todos los estilos fueron añadidos mediante TailwindCSS para familiarizarse más con este framework.',
+      desc4:
+        'Una lista de cosas por hacer implementando hooks personalizados, creando filtros de visibilidad para ver lo completado y lo que esta aun por hacer, usando el hook useContext y stores debito a los estados más complejos, usando dispatch en lugar de callbacks para mejor rendimiento en la aplicación. Se una Framer-motion para suavizar las transiciones entre los estados y hacerlo más atractivo al usuario.',
+      desc5:
+        'Este proyecto de bienes raíces se enfoca en el uso del enrutamiento basado en archivos que nos provee NextJS, haciendo el enrutamiento fácil de implementar y entender. Se usa Chakra UI para conocer la librería, herramientas como nprogress para mostrar el progreso en pantallas de carga de la aplicación. Se hace énfasis en el uso de las funciones getStaticProps y getServerSideProps para pasar props a los componentes en entornos estáticos o dinámicos .'
     }
   }
 }
